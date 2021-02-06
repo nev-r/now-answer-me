584b790461d70810ea55c6c5af2d5f7c686e14e1
nev <gh@f-m.fm>
2021-02-06 09:39:16 -0800

    more exports

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,1 +1,2 @@
 export { buildEmojiDict, uploadEmojis } from "./_bot/emoji-management.js";
+export { resolveChannel, resolveEmoji, resolveGuild } from "./_bot/resolvers.js";
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,4 @@
 //
 
 export { buildEmojiDict, uploadEmojis } from "./_bot/emoji-management.js";
+export { resolveChannel, resolveEmoji, resolveGuild } from "./_bot/resolvers.js";
