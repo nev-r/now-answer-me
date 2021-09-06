9b35f13cc4f0fff070acec7c0c767c32c3779385
nev <gh@f-m.fm>
2021-09-06 07:50:16 -0700

    wip message components

--- features/index.js
+++ features/index.js
@@ -2,3 +2,4 @@ export { buildReactionFilter } from "../utils/reactionHelpers.js";
 export { delMsg, bugOut } from "../utils/misc.js";
 export { presentOptions, promptForText } from "../utils/user-input.js";
 export { sendPaginatedEmbed, sendPaginatedSelector, sendRerollableEmbed, sendRerollableStackEmbed, } from "./interactive-embeds.js";
+export { createPaginator, } from "./component-paginated-embeds.js";
