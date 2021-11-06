8eafbe06a1779f2b2f1f664fb1640fb166fd4fb6
nev <gh@f-m.fm>
2021-11-05 20:49:08 -0700

    dynamic emoji manager

--- features/index.d.ts
+++ features/index.d.ts
@@ -3,3 +3,4 @@ export { delMsg, bugOut } from "../utils/misc.js";
 export { presentOptions, promptForText } from "../utils/user-input.js";
 export { sendPaginatedEmbed, sendPaginatedSelector, sendRerollableEmbed, sendRerollableStackEmbed, } from "./interactive-embeds.js";
 export { createPaginator, createPaginatedSelector } from "./component-paginated-embeds.js";
+export { createDynamicEmojiManager } from "./dynamic-emoji-manager.js";
